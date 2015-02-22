03ac7bfa5d09d335467cae953b8d51bfbec6e3ca
Tommi Liittokivi <tliittok@gmail.com>
2015-02-22 13:38:28 +0200

    Added documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,33 @@
+# Functions to compute matrix inversion once and cache it's value for repeated calls
+#
+# Example:
+# > m = makeCacheMatrix(matrix(c(5,5,6,7),2,2))
+#
+# > m$get()
+# [,1] [,2]
+# [1,]    5    6
+# [2,]    5    7
+#
+# > m$getinverse()
+# NULL
+#
+# > i = cacheSolve(m)
+# > i
+# [,1] [,2]
+# [1,]  1.4 -1.2
+# [2,] -1.0  1.0
+#
+# > identical(i, m$getinverse())
+# [1] TRUE
 
 
+
+
+# Returns list of getter/setter functions that can be used to store matrix and it's inverse matrix
+#
+# Arguments:
+# x    A square numeric or complex matrix
+#
 makeCacheMatrix <- function(x = matrix()) {
     # variable for caching the inverse matrix
     inverse <- NULL
@@ -30,7 +58,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+# Returns inverse of given matrix. If inverse has been computed before for this object, 
+# cached value is returned.
+#
+# Arguments:
+# x    Matrix object created with makeCacheMatrix function
+#
 cacheSolve <- function(x, ...) {
     # try to get cached inverse
     inverse <- x$getinverse()
